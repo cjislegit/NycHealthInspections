ac0fe2d18bd94363adc23aa2fda6e85122d46544
cjislegit <carlosjramirez11@gmail.com>
2025-03-15 12:47:41 -0700

    Initial Commit

---
+++ NycHealthInspections/NycHealthInspectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NycHealthInspectionsApp.swift
+//  NycHealthInspections
+//
+//  Created by Carlos Ramirez on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct NycHealthInspectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
